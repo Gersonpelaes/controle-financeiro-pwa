31a9fde7dd27bb0a5e0f2e56898c3d97cbd8762a
Gersonpelaes <gersonpelaes@gmail.com>
2025-07-06 12:12:34 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 2ceb487dca4c9caf50debf58b567919b30fcb3f5
