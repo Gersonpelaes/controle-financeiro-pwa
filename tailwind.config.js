0370793b818c23becaef1844ae2dfd99370fe9af
Gersonpelaes <gersonpelaes@gmail.com>
2025-07-06 12:14:16 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,16 +9,3 @@ export default {
   },
   plugins: [],
 }
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> 2ceb487dca4c9caf50debf58b567919b30fcb3f5
